e08222592f22aa9c40ae488c33f79a6b367bc5c1
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2020-07-23 18:58:19 +0400

    added pump control cgi

---
+++ 01-pump.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+static void gpio_op(uint8_t* reg_base)
+{
+    switch(getchar()) {
+        case -1:
+            return;
+        case '0':
+            reg_base[0x644] |= 0b100000;
+            break;
+        case '1':
+            reg_base[0x634] |= 0b100000;
+            break;
+    }
+    printf("Access-Control-Allow-Origin: *\r\nContent-type: text/plain, charset=utf-8\r\n\r\n");
+    printf("%d\n", !!(reg_base[0x624] & 0b100000));
+}
+
+int main()
+{
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if(fd == -1)
+        return 2;
+
+    uint8_t* reg_base = mmap(0, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x10000000);
+    close(fd);
+
+    if((void*)-1 == reg_base) {
+        perror("mmap");
+        return 3;
+    }
+
+    gpio_op(reg_base);
+
+    munmap((void*)reg_base, 0x1000);
+    return 0;
+}
+
--- Makefile
+++ Makefile
@@ -3,18 +3,23 @@ CC=mipsel-openwrt-linux-gcc
 DEB=-Wall -g
 REL=-Wall -O3 -s
 
-deb: 00-deb
-rel: 00-rel
+deb: 00-deb 01-deb
+rel: 00-rel 01-rel
 
 00-deb:
 	$(CC) $(DEB) -o 00-sens 00-sens.c
 00-rel:
 	$(CC) $(REL) -o 00-sens 00-sens.c
 
+01-deb:
+	$(CC) $(DEB) -o 01-pump 01-pump.c
+01-rel:
+	$(CC) $(REL) -o 01-pump 01-pump.c
+
 tags:
 	ctags -R . $(STAGING_DIR)/toolchain-mipsel_24kc_gcc-7.3.0_musl/include
 
 clean:
-	rm -f 00-sens
+	rm -f 00-sens 01-pump
 	rm -f tags
 
